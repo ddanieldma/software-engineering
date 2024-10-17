b79cdbfbedd40bfe1510a902b819c71ea0ff3d2d
ddanieldma <danielalmemiranda@gmail.com>
2024-10-17 05:08:06 -0300

    adiciona pagina de detalhe do produto

--- src/webpage/get_vending_machines.py
+++ src/webpage/get_vending_machines.py
@@ -4,9 +4,6 @@ import os
 # Importando module com caminho relativo
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-# Preciso receber uma lista de produtos e exibir essa lista de uma forma organizada no site.
-# Vou criar uma lista com 3 produtos e colocá-los em uma estrutura bem simples no site com flexbox.
-
 import vending as v
 
 # Get all vending machines
