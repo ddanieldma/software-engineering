478f4bd61e136567a4413d07f6548550113592ed
ddanieldma <danielalmemiranda@gmail.com>
2024-10-16 23:48:18 -0300

    split test cases for empty and non-empty product lists

--- src/test/test_vending.py
+++ src/test/test_vending.py
@@ -8,10 +8,17 @@ import unittest
 from vending import get_products_from_db, Product
 
 class TestProductDatabase(unittest.TestCase):
-    def test_get_products_from_db(self):
+    def test_get_empty_product_list(self):
         products = get_products_from_db()
         self.assertIsInstance(products, list)
         self.assertEqual(len(products), 0)
+
+    def test_getting_non_empty_list(self):
+        products = get_products_from_db()
+        self.assertEqual(len(products), 2)
+        self.assertIsInstance(products[0], Product)
+        self.assertEqual(products[0].get_name(), "Coke")
+        self.assertEqual(products[0].get_price(), 8)
 
 if __name__ == '__main__':
     unittest.main()
